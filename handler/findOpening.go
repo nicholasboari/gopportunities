b25b42b407a0f8390008f6e3d738df386810db73
nicholasboari <nicholasboari@gmail.com>
2023-09-12 10:44:59 -0300

    feat: add find opening handler logic

--- handler/findOpening.go
+++ handler/findOpening.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nicholasboari/gopportunities/schemas"
 )
 
 func FindOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "string").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, http.StatusOK, "show-opening", opening)
 }
