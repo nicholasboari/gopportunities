0ae264745a68436a853f54926e9cfad05e113c57
nicholasboari <nicholasboari@gmail.com>
2023-09-12 19:14:23 -0300

    refact: opening structure

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,15 +1,29 @@
 package schemas
 
 import (
-	"gorm.io/gorm"
+	"time"
 )
 
 type Opening struct {
-	gorm.Model
-	Role     string `json:"role"`
-	Company  string `json:"company"`
-	Location string `json:"location"`
-	Remote   bool   `json:"remote"`
-	Link     string `json:"link"`
-	Salary   int64  `json:"salary"`
+	ID        uint      `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }
